97f9963bbe92bf97d35e9140b7a974e8cd1ed1a4
Alejandro <tqaw19@gmail.com>
2019-09-12 13:08:27 -0500

    creacion modelos

--- src/main/java/com/tecsup/lab01/controller/UserController.java
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -1,5 +1,12 @@
 
 package com.tecsup.lab01.controller;
+
+import com.tecsup.lab01.model.User;
+
 public class UserController {
+    User u=new User();
+    String nombre;
+    
+    
     
 }
--- src/main/java/com/tecsup/lab01/model/User.java
+++ src/main/java/com/tecsup/lab01/model/User.java
@@ -1,5 +1,32 @@
 
 package com.tecsup.lab01.model;
 public class User {
+    String nombres;
+    String apellido_paterno;
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellido_paterno() {
+        return apellido_paterno;
+    }
+
+    public void setApellido_paterno(String apellido_paterno) {
+        this.apellido_paterno = apellido_paterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+     int edad;
     
 }
